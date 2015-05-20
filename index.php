e36c0f2a8009cc663935b187826495fa227b4661
Polochon-street <polochonstreet@gmx.fr>
2015-05-20 11:30:07 +0200

    Added an « around me » function

--- index.php
+++ index.php
@@ -4,11 +4,38 @@
 
 <h1>Bienvenue !</h1>
 
- <p>
-        <form name="inscription" method="post" action="map.php">
-        Destination (nom du monument) : <input type="text" name="name"/>
-        <input type="submit" name="valider" value="Go !"/>
-        </form>
+<p>
+ 	<button onclick="getLocation()">Try It</button>
+	<script>
+	function openWithPostData(page,data) {
+		var form = document.createElement('form');
+    	form.setAttribute('action', page);
+    	form.setAttribute('method', 'post');
+    	for (var n in data) {
+        	var inputvar = document.createElement('input');
+        	inputvar.setAttribute('type', 'hidden');
+        	inputvar.setAttribute('name', n);
+        	inputvar.setAttribute('value', data[n]);
+        	form.appendChild(inputvar);
+    	}
+    document.body.appendChild(form);
+    form.submit();
+	}
+	function getLocation() {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(showPosition);
+		} else {
+		x.innerHTML = "Geolocation is not supported by this browser.";
+		}
+	}
+	function showPosition(position) {
+		var latlong = new Array();
+
+		latlong[0] = position.coords.latitude;
+		latlong[1] = position.coords.longitude;
+		openWithPostData("map.php", latlong);
+	}
+	</script>   
 </p>
 
 <?php
--- map.php
+++ map.php
@@ -1,73 +1,81 @@
 <?php
+	include("./include/haut_map.php"); 
 
-include("./include/haut_map.php");
- 
-#function strpos_offset($needle, $haystack, $occurrence) {
-  // explode the haystack
-#  $arr = explode($needle, $haystack);
-  // check the needle is not out of bounds
-#  switch( $occurrence ) {
-#    case $occurrence == 0:
-  #    return false;
- #   case $occurrence > max(array_keys($arr)):
-   #   return false;
-   # default:
-   #   return strlen(implode($needle, array_slice($arr, 0, $occurrence)));
-  #}
-#}     
-        // Même chose que error_reporting(E_ALL);
-        ini_set('error_reporting', E_ALL);
-        $name = $_POST['name'];
-        $dom2 = new DomDocument();
-        $dom1 = new DomDocument();
-        $dom3 = new DomDocument();
-        $dom4 = new DomDocument();
- 
-        $dom1->load("http://www.wikidata.org/w/api.php?action=wbsearchentities&search=$name&language=fr&format=xml");
- 
-        $listvalues = $dom1->getElementsByTagName('entity');
-        if ($listvalues->length!=0) {
- 
-        $value = $listvalues->item(0);
-        $id = $value->getAttribute("id");
-        $description = $value->getAttribute("description");
- 
- 
-        $dom2->load("http://www.wikidata.org/w/api.php?action=wbgetclaims&format=xml&entity=$id&property=P625");
-        $listvalues = $dom2->getElementsByTagName('value');
-        if ($listvalues->length!=0) {
-        $value = $listvalues->item(0);
-        $latitude = $value->getAttribute("latitude");
-        $longitude = $value->getAttribute("longitude");
- 
-        $dom3->load("http://www.wikidata.org/w/api.php?action=wbgetentities&ids=$id&sitefilter=frwiki&props=sitelinks/urls&format=xml");
-        $listvalues = $dom3->getElementsByTagName('sitelink');
-        $value = $listvalues->item(0);
-        $url = $value->getAttribute("url");
- 
-        //echo ($listvalues[0])->getAttribute("latitude");
-        //$latitude = $coordinxml->getAttribute("latitude");
-        }
-        }
-        ?>
- 
-        <!--<?php echo "Latitude: $latitude"; ?> <br />
-        <?php echo "Longitude: $longitude"; ?> <br />
-        <?php echo "Description: $id"; ?> <br />
-        <a href=<?php echo "http://www.openstreetmap.org/#map=17/$latitude/$longitude"; ?>>OSM</a>
-        -->
-        </p>
-        <div id='map'></div>
- 
-	<script>
-		   
-		L.mapbox.accessToken = 'pk.eyJ1IjoicG9sb2Nob24tc3RyZWV0IiwiYSI6Ikh5LVJqS0UifQ.J0NayavxaAYK1SxMnVcxKg';
-		var map = L.mapbox.map('map', 'polochon-street.kpogic18')
-		.setView([<?php echo "$latitude";?>, <?php echo "$longitude";?>], 18);
-		var marker = L.marker([<?php echo "$latitude";?>, <?php echo "$longitude";?>]).addTo(map);
-		marker.bindPopup("<?php echo "$description";?> <br /> <p><a target=\"_blank\" href=\"http:<?php echo "$url";?>\">Lien wikipedia</a> <br /> <a href=\"http://perdu.com\">[+]</a></p>").openPopup();
-	</script>
+error_reporting(E_ALL);
+//	$name = $_POST['name'];
+
+	/* Encode array and get informations about the page we want (id, description, etc) */
+/*	$search_array_json = file_get_contents("http://www.wikidata.org/w/api.php?action=wbsearchentities&search=" . urlencode($name) . "&language=fr&format=json");
+	$search_array = json_decode($search_array_json, true);
+	$id = $search_array["search"][0]["id"];
+	$description = $search_array["search"][0]["description"]; */
+
+	/* Encode array and get informations about the localisation of that page (lat/long/height, etc) */
+	/* $geoloc_array_json = file_get_contents("http://www.wikidata.org/w/api.php?action=wbgetclaims&format=json&entity=$id&property=P625");
+	$geoloc_array = json_decode($geoloc_array_json, true);
+
+	$latitude = $geoloc_array["claims"]["P625"][0]["mainsnak"]["datavalue"]["value"]["latitude"];
+	$longitude = $geoloc_array["claims"]["P625"][0]["mainsnak"]["datavalue"]["value"]["longitude"]; */
+	
+	/* Encode array and get fr wikipedia link about that page */
+	/*$sitelink_array_json = file_get_contents("http://www.wikidata.org/w/api.php?action=wbgetentities&ids=$id&sitefilter=frwiki&props=sitelinks/urls&format=json");
+	$sitelink_array = json_decode($sitelink_array_json, true);
+	$sitelink = $sitelink_array["entities"]["$id"]["sitelinks"]["frwiki"]["url"]; */
+	$latitude = $_POST[0];
+	$longitude = $_POST[1];
+
+	$poi_array_json = file_get_contents("http://wdq.wmflabs.org/api?q=around[625,$latitude,$longitude,1]");
+	$poi_array = json_decode($poi_array_json, true);
+	$poi_array_clean = $poi_array["items"];
+	$nb_poi = count($poi_array_clean);
+	
+	//echo($poi_array_clean[4]);
+	for($i = 0; $i < 10; $i++) {
+		$temp_geoloc_array_json = file_get_contents("http://www.wikidata.org/w/api.php?action=wbgetclaims&format=json&entity=Q" . $poi_array_clean["$i"] . "&property=P625");
+		$temp_geoloc_array = json_decode($temp_geoloc_array_json, true);
+
+		$temp_latitude = $temp_geoloc_array["claims"]["P625"][0]["mainsnak"]["datavalue"]["value"]["latitude"];
+		$temp_longitude = $temp_geoloc_array["claims"]["P625"][0]["mainsnak"]["datavalue"]["value"]["longitude"];
+
+		$temp_description_array_json = file_get_contents("http://www.wikidata.org/w/api.php?action=wbgetentities&format=json&ids=Q" . $poi_array_clean["$i"] . "&props=labels&languages=fr");
+		$temp_description_array = json_decode($temp_description_array_json, true);
+		$name = $temp_description_array["entities"]["Q" . $poi_array_clean["$i"]]["labels"]["fr"]["value"];
+
+		$temp_sitelink_array_json = file_get_contents("http://www.wikidata.org/w/api.php?action=wbgetentities&ids=Q" . $poi_array_clean["$i"] . "&sitefilter=frwiki&props=sitelinks/urls&format=json");
+		$temp_sitelink_array = json_decode($temp_sitelink_array_json, true);
+		$temp_sitelink = $temp_sitelink_array["entities"]["Q" . $poi_array_clean["$i"]]["sitelinks"]["frwiki"]["url"];
+	
+		$poi_array_ll[$i]["latitude"] = $temp_latitude;
+		$poi_array_ll[$i]["longitude"] = $temp_longitude;
+		$poi_array_ll[$i]["name"] = $name;
+		$poi_array_ll[$i]["sitelink"] = $temp_sitelink;
+	}
+//	echo($poi_array_ll["latitude"][4]);
+//	echo("\n");
+//	echo($poi_array_ll["longitude"][4]);
+	$poi_array_json_encoded = json_encode((array)$poi_array_ll);
+?>
+
+<div id="map" class="map"></div>
+<script>
+	var poi_array = new Array();
+	poi_array = <?php echo($poi_array_json_encoded); ?>;
+	L.mapbox.accessToken = 'pk.eyJ1IjoicG9sb2Nob24tc3RyZWV0IiwiYSI6Ikh5LVJqS0UifQ.J0NayavxaAYK1SxMnVcxKg';
+	var map = L.mapbox.map('map', 'polochon-street.kpogic18')
+    //.setView([<?php echo "$latitude" ?>, <?php echo "$longitude" ?> ], 18);
+		
+	var marker = L.marker([poi_array[0].latitude, poi_array[0].longitude], {opacity:0.5}).addTo(map);
+	marker.bindPopup(poi_array[0].name + "<br /> <p><a target=\"_blank\" href=\"http:" + poi_array[0].sitelink + "\">Lien wikipédia</a> <br /> <a href=\"http://perdu.com\">[+]</a></p>").openPopup();
+	for(i = 1; i < 10; ++i) {
+		var marker = L.marker([poi_array[i].latitude, poi_array[i].longitude]).addTo(map); 
+	//	marker.bindPopup(poi_array[i].name).openPopup();
+		marker.bindPopup(poi_array[i].name + "<br /> <p><a target=\"_blank\" href=\"http:" + poi_array[i].sitelink + "\">Lien wikipédia</a> <br /> <a href=\"http://perdu.com\">[+]</a></p>").openPopup();
+	}
+	//marker.bindPopup("<?php echo "$description";?> <br /> <p><a target=\"_blank\" href=\"http:<?php echo "$sitelink";?>\">Lien wikipédia</a> <br /> <a href=\"http://perdu.com\">[+]</a></p>").openPopup();
+	map.setView([<?php echo "$latitude" ?>, <?php echo "$longitude" ?> ], 16);
+</script>
+
+
 <?php
-
-include("./include/bas.php");
+	include("./include/bas.php");
 ?>
