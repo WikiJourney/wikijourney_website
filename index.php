5e7ed894d36d7b5f4ccbd01c66f874c31059a5ae
sylvainar <sylvain.ar@hotmail.fr>
2015-05-24 19:09:58 +0200

    Style on index's form

--- index.php
+++ index.php
@@ -7,20 +7,24 @@
 <p>
 	<form method="post" action="map.php" id="formPOI">
 		
-		<input type="radio" name="choice" value="adress" checked>Close to an adress !
-		<input type="text" value="Type your adress here." name="adressValue" id="adressValue" />
+		<input type="radio" name="choice" value="adress" checked>Close to a location !
+		<p>
+		<input type="text" value="Type a location here" name="adressValue" id="adressValue" /></p>
 		<br/>
-		<input type="radio" name="choice" value="around" onclick="getGeolocation(); ">Around me ! (Not working with Safari for the moment..)
+		<input type="radio" name="choice" value="around" onclick="getGeolocation(); ">Around me ! 
 		<br/>
-				<input type="text" name="longitude" id="longitude" value="" readonly />
-				<input type="text" name="latitude" id="latitude" value="" readonly />
-		<br/><br/>
+		<p style="font-size: 12px; width: 50%; margin-left: 10%">Note : This fonctionnality involve geolocation and could not work, it
+		depends of the device you're on and the way you're connected to the internet.</p>
+		<br/>
+		<br/>
 		
-		Options :<br/>
-		<label for="range">Range (km) : </label><input type="text" name="range" id="range" value="1"/><br/>
-		<label for="maxPOI">Max POI : </label><input type="text" name="maxPOI" id="maxPOI" value="10" /><br/><br/>
-		
+		Options :<p>
+		<label for="range">Range (km) : </label><input class="miniInput" type="text" name="range" id="range" value="1"/><br/>
+		<label for="maxPOI">Max POI : </label><input class="miniInput" type="text" name="maxPOI" id="maxPOI" value="10" /><br/><br/>
+		</p>
 
+		<input type="hidden" name="longitude" id="longitude" value="" readonly />
+		<input type="hidden" name="latitude" id="latitude" value="" readonly />
 		
 		<input type="submit" value="Go !" />
 		
