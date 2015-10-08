ba5229785c595eb2b695dbf4ef6dc245ed513730
Neako <maesneako@gmail.com>
2015-10-08 20:50:10 +0200

    link to blog

--- include/bas.php
+++ include/bas.php
@@ -14,7 +14,8 @@
 						<a href="https://www.facebook.com/WikiJourney"><img src="./images/design/fb.png" alt="Facebook" title="Facebook" /></a>
 						<a href="https://twitter.com/WikiJourney"><img src="./images/design/twitter.png" alt="Twitter" title="Twitter" /></a>
 						<a href="https://github.com/WikiJourney/"><img src="./images/design/github.png" alt="GitHub" title="GitHub" /></a>
-						<a href="https://play.google.com/apps/testing/com.wikijourney.wikijourney"><img src="./images/design/google_play.png" alt="Become an alpha tester !" title="Become an alpha tester !" /></a>
+						<a href="https://play.google.com/apps/testing/com.wikijourney.wikijourney"><img src="./images/design/google_play.png" alt="Become an alpha tester!" title="Become an alpha tester!" /></a>
+						<a href="http://blog.wikijourney.eu"><img src="./images/design/pluxml.png" alt="Our Blog!" title="Our Blog!" /></a>
 					</td>
 						
 					<!--<td>Blabla</td>-->
--- technical.php
+++ technical.php
@@ -7,6 +7,8 @@
 <p><?php echo _TECHNICAL_TEXT; ?></p>
 
 <p>Wiki : <a href="https://github.com/WikiJourney/wikijourney_website/wiki" title="Wiki">GitHub</a></p>
+<p>Blog : <a href="http://blog.wikijourney.eu" title="Blog">Blog</a></p>
+
 
 <?php
 	include("./include/bas.php");
