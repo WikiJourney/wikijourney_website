565f1bb2b177a0bb23c6fcab56782069619cf8aa
sylvainar <sylvain.ar@hotmail.fr>
2015-10-28 14:33:15 +0100

    Changed Piwik tracker

--- include/haut.php
+++ include/haut.php
@@ -74,14 +74,14 @@ else
 	  _paq.push(['trackPageView']);
 	  _paq.push(['enableLinkTracking']);
 	  (function() {
-	    var u="//149.202.52.36/piwik/";
+	    var u="//piwik.wikijourney.eu/";
 	    _paq.push(['setTrackerUrl', u+'piwik.php']);
 	    _paq.push(['setSiteId', 1]);
 	    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
 	    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
 	  })();
 	</script>
-	<noscript><p><img src="//149.202.52.36/piwik/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
+	<noscript><p><img src="//piwik.wikijourney.eu/piwik.php?idsite=1" style="border:0;" alt="" /></p></noscript>
 	<!-- End Piwik Code -->
 	
 	<div id="banniere">
